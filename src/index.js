1f3f0a5d94f8d6d22eacd6c324a289d31ec04653
roadlampR <50226763+roadlampR@users.noreply.github.com>
2024-12-26 16:28:27 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.roadlamp.top" + CUSTOM_DOMAIN]: dockerHub,
+  ["quay.roadlamp.top" + CUSTOM_DOMAIN]: "https://quay.io",
+  ["gcr.roadlamp.top" + CUSTOM_DOMAIN]: "https://gcr.io",
+  ["k8s-gcr.roadlamp.top" + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  ["k8s.roadlamp.top" + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["ghcr.roadlamp.top" + CUSTOM_DOMAIN]: "https://ghcr.io",
+  ["cloudsmith.roadlamp.top" + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr.roadlamp.top" + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
